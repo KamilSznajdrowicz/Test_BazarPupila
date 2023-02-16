a9729fddc83dff1af27f5ab1f1500e412fd94323
KamilSznajdrowicz <tester@tester-m>
2023-02-16 20:46:50 +0000

    Adding headless option to chrome browser in comment

--- Tests/test_base.py
+++ Tests/test_base.py
@@ -4,13 +4,19 @@ from Pages.home_page import HomePage
 
 class BaseTest(unittest.TestCase):
     def setUp(self):
-        print("Przygotowanie testu") #warunki wstepne
+        print("Przygotowanie testu") #preconditions
         self.driver = webdriver.Chrome()
         self.driver.get("https://bazarpupila.pl/")
         self.driver.maximize_window()
         self.driver.implicitly_wait(10)
         self.home_page = HomePage(self.driver)
 
+        #Headless option
+        # chrome_options = Options()
+        # chrome_options.add_argument("--window-size=1920,1080")
+        # chrome_options.add_argument("--start-maximized")
+        # chrome_options.add_argument("--headless")
+        # self.driver = webdriver.Chrome(options=chrome_options)
     def tearDown(self):
         print("Zakonczenie testu")
         self.driver.quit()
